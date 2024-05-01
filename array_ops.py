311d3900c91e1cf7cda81b5c3203b6dc3fb0e4eb
leulshawel <leulshawel12@gmail.com>
2024-05-01 23:08:04 +0300

    Interesting :)

--- array_ops.py
+++ array_ops.py
@@ -1,11 +1,14 @@
 #some usable funcs and classes not directly related to any object
 from typing import List, Union, Tuple
 from dtype import Dtype
+import math
 
 
 def get_shape(data: List[Union[int, float, bool, List]]): pass
 #from tinygrad.tensor
 def flatten(l: Union[List, Tuple]): return [item for sublist in l for item in (flatten(sublist) if isinstance(sublist, (tuple, list)) else [sublist])]
+#get the number of elements of a tensor
+def num_of_elems(l: Union[Tuple, List]): return math.prod(l)  
 #generate an array from a shape
 def array_from_shape(shape: Union[List, tuple, int], val=0): return [val for _ in range(shape[0])] if len(shape)==1 \
     else [array_from_shape(shape[1:], val) for _ in range(shape[0])]
@@ -13,9 +16,17 @@ def array_from_shape(shape: Union[List, tuple, int], val=0): return [val for _ i
 def shape_from_array(l: Union[List, Tuple]): return shape_from_array(l[0]) + [len(l)] if isinstance(l[0], (tuple, list)) else [len(l)]
 #add a constant value on every element of a list
 def add_const(l: Union[List, Tuple], c: Union[int, float, bool, Dtype]): return [add_const(e, c) if isinstance(e, (list, tuple)) else e+c for e in l]
+#multipuly with a scalar
+def scale(l: Union[List, Tuple], c: Union[int, float, bool, Dtype]): return [add_const(e, c) if isinstance(e, (list, tuple)) else e*c for e in l]
 #set every element to a value
 def set (l: Union[List, Tuple], val: Union[int, float, bool, Dtype]=0): return [set(e, val) if isinstance(e, (list, tuple)) else val for e in l]
 #normal list.reverse() doesn't return
 def reverse(l): 
     l.reverse()
     return l
+
+#devide a list in to n number if lists of equak size
+def devid_list(l, n):
+    assert not len(l) % n, f"array not devisible in to equal {n} arrays"
+    ne = int(len(l)/n)
+    return [l[i*ne:ne*(i+1)] for i in range(n)]
--- tensor.py
+++ tensor.py
@@ -3,12 +3,19 @@ from typing import Optional, Union, List
 
 from dtype import Dtype
 from array_ops import flatten, array_from_shape, add_const,\
-    shape_from_array, reverse, num_of_elems
+    shape_from_array, reverse, num_of_elems, scale, devid_list
 from ops import binary_op_wrapper, unary_op_wrapper
 
 class Tensor():
-    __slots__ = "data", "shape", "dtype", "ctx", "strong" #No More attributes
-                 
+    __slots__ = "data", "shape", "dtype", "ctx", "strong", "mat" #No More attributes
+    class Matrix:
+        #TODO: implement matrix multiplication
+        def matmul(self, other):
+            assert isinstance(other, Tensor.Matrix),\
+                "matrix multiplication is only defined for 2D Tensors"
+            pass
+        def transpose(self):
+            return self
     def __init__(self, data: Optional[Union[None, np.ndarray, List, int, float, bool]]=None, 
         shape: tuple[int]=None, dtype: Optional[Dtype]=None, ctx = None, strong: bool=True):
         assert dtype is None or isinstance(dtype, Dtype), "dtype unknown"
@@ -20,20 +27,31 @@ class Tensor():
             if isinstance(data, (list, tuple)): self.data, self.shape = flatten(data), reverse(shape_from_array(data))
             else: raise RuntimeError("Tensor creation failed")
         self.dtype = dtype
+        self.mat = Tensor.Matrix() if len(self.shape) == 2 else None
         
     @classmethod
     def new(cls, data=None, shape=None, dtype=None, ctx=None, strong=None): return cls(data, shape, dtype, ctx, strong)
+    #get the data with right dimension (unflatten)
+    def data_with_dim(self, l=None, n=0): 
+        if not l: l = self.data
+        if n+1 == len(self.shape): return l
+        return [self.data_with_dim(dl, n+1) for dl in devid_list(l, self.shape[n])]  
+            
     #on self or return binary ops
-    @binary_op_wrapper
+    @binary_op_wrapper  
     def __add__(self, other): return [x+y for x, y in zip(self.data, other.data)]
     @binary_op_wrapper
     def __sub__(self, other): return [x-y for x, y in zip(self.data, other.data)]
     @binary_op_wrapper
     def __mul__(self, other): return [x*y for x, y in zip(self.data, other.data)]
+    def matmul(self, other):  return Tensor(data=self.mat.matmul(other.mat)) 
     
     #on self or return unaryOps
     @unary_op_wrapper
     def sadd(self, s): return add_const(self.data, s)
+    #on self or return unaryOps
+    @unary_op_wrapper
+    def smul(self, s): return scale(self.data, s)
     @unary_op_wrapper
     def __sqr__(self): return self * self
     @unary_op_wrapper
@@ -46,15 +64,13 @@ class Tensor():
     #reshaping the tensor is simple
     def reshape(self, shape):
         assert num_of_elems(self.shape) == num_of_elems(shape), "Tensor can't be of this shape"
+        self.mat = Tensor.Matrix() if len(shape) == 2 else None
         self.shape = shape
     
     #TODO: reduce Ops  
     #dot product
-    def dot(self, other): pass
+    def dot(self, other, axis): return []
     #add elemens in a single dimension
     def sum(self, dim): pass
-    #multopy two tensors
-    def matmul(self, other):
-        shape = self.shape
+    #elemnt-wise multiplication
     def mul(self, other): return self * other
-    
